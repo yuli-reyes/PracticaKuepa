156f3179d3b87ce307b40d46977019bd50183316
unknown <viviana1236@gmail.com>
2021-07-23 19:42:14 -0500

    Se agregó footer al login

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,15 @@
+import './footer.css';
+
+
+function Footer(){
+  return(
+
+    <div>    
+      <footer>
+        <p>&copy; Sitio desarrollado por  Kuepa EduTech | Todos los derechos reservados.</p>
+      </footer>
+    </div> 
+    );
+}
+
+export default Footer;
